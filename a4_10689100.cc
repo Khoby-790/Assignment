75266c4f831e5011adbb88d7c19857a817e8c7f8
unknown <ebaidoo79@gmail.com>
2018-09-12 21:14:13 +0100

    assignment proto

---
+++ a4_10689100.cc
@@ -0,0 +1,43 @@
+//Assignment 4 using the Euclidean algorithm in a recursive fuction to compute the gcd
+//of two numbers
+#include <iostream>
+using namespace std;
+//decalare prototype of functions
+int calcutateG_C_D(int , int);
+
+int main(int argc, char const *argv[]) {
+  //local variables
+  int number_1,number_2;
+  cout << "Calculate The GCD of two numbers" << endl;
+  cout << "Enter the first number :: ";
+  cin >>number_1;
+  cout << "Enetr the second number :: ";
+  cin >> number_2;
+
+  cout << "The Greatest Commmon Divisor of " << number_1 << " and " << number_2 << " is " << calcutateG_C_D(number_1,number_2) << endl;
+  return 0;
+}
+
+
+
+int calcutateG_C_D(int firstNumber, int secondNumber){
+  //local variables
+  int remainder;
+  //perform check to determine which number is greater
+
+  if(secondNumber > firstNumber){
+    calcutateG_C_D(secondNumber,firstNumber);
+  }else{
+    //compute the mod of the two numbers to get the remainder
+    remainder = firstNumber % secondNumber;
+    //check if the remainder is 0
+    if(remainder == 0){
+      //if remainder is 0
+      return secondNumber;
+    }
+    //else
+    firstNumber = firstNumber;
+    secondNumber = remainder;
+    return calcutateG_C_D(firstNumber, secondNumber);
+  }
+}
